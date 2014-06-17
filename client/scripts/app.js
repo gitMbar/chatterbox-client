6c046a162a1f3b2e1adb52cc26bd48113e4729d6
Hack Reactor Students <mbarton.mail@gmail.com>
2014-06-16 20:26:49 -0700

    Added messaging, room selection, and add new room functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,135 @@
 // YOUR CODE HERE:
+var app = {
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  rooms: {},
 
+  init: function(){
+    this.message = {
+      username: this.findUsername(),
+      text: '',
+      roomname: null
+    };
+    this.fetch();
+
+    $("form").on("keypress", "#msgtext", function(e){
+      if (e.which === 13){
+        e.preventDefault();
+        app.message.text = $(this).val();
+        app.send(app.message);
+        $(this).val("");
+        app.fetch();
+      }
+    });
+
+    $("form").on("keypress", "#roomtext", function(e){
+      if (e.which === 13){
+        e.preventDefault();
+        var newRoom = $(this).val();
+        app.createRoom(newRoom);
+        $(this).val("");
+      }
+    });
+
+    $("#roomChooser").on("change", function(){
+      app.message.roomname = $(this).val();
+      app.fetch();
+    })
+
+    //setInterval
+
+  },
+  send: function(message){
+    $.ajax({
+      url: app.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(data){
+        console.log('data is: ' , data);
+      },
+      error: function(data){
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+  fetch: function(){
+    $.ajax({
+      url: app.server + "?order=-createdAt",
+      type: 'GET',
+      success: function(data){
+        console.log('data is: ' , data);
+        app.getRooms(data.results);
+        if (app.message.roomname !== null){
+         app.display(data.results);
+        }
+      },
+      error: function(data){
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+
+  display: function(data){
+    console.log(data);
+    var counter = 0;
+    this.clearMessages();
+    for (var i = 0; i < data.length; i++){
+      if (data[i].roomname === app.message.roomname){
+        app.addMessage(data[i]);
+        counter++;
+      }
+      if (counter >=15){
+        break;
+      }
+
+    }
+
+  },
+
+  findUsername: function(){
+    if (/(&|\?)username=/.test(window.location.search)) {
+      return window.location.search.slice(10);
+    }
+
+
+  },
+
+  addMessage: function(msgObject){
+    var message = "<li>" + escape(msgObject.username) + ": " + escape(msgObject.text) + "</li>";
+    $('.chat').append(message);
+  },
+
+  clearMessages: function(){
+     $(".chat").children().remove();
+  },
+
+  getRooms: function(data){
+    //note: rooms will last foreve
+    $('#roomChooser').children().remove();
+    for (var i = 0; i < data.length; i++){
+      if (app.rooms[data[i].roomname] === undefined){
+        app.rooms[data[i].roomname] = true;
+      }
+    }
+    //TODO: check if room has a value and set that prop to selected
+    for (var key in app.rooms){
+      var selectStatus = '';
+      if (key === app.message.roomname){
+        selectStatus = " selected=\"selected\" ";
+      }
+        $('#roomChooser').append("<option " + selectStatus+ ">" + key + "</option>");
+        selectStatus = '';
+    }
+  },
+
+  createRoom: function(roomname){
+    app.message.roomname = roomname;
+    $('#roomChooser').append("<option>" + roomname + "</option>");
+    $('#roomChooser').val(roomname);
+    app.clearMessages()
+  }
+  //getUsers: function(){}
+};
+
+$(document).ready(function(){
+  app.init();})
